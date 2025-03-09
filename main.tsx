a4ce04969bcbd673fb8cd11356d528bb28002403
RayyanNafees <nafees.rayyan@gmail.com>
2025-03-09 06:34:37 +0530

    added report and Registeration card download

--- main.tsx
+++ main.tsx
@@ -5,11 +5,11 @@ import getInfoFromCard from "./student.ts";
 const app = new Hono();
 
 app.get("/", async (c) => {
-	const enroll = c.req.query("enroll") ?? "GP4519";
+	const enroll = c.req.query("enroll") ?? "";
 
-	const studentInfo = await getInfoFromCard(enroll.toUpperCase());
+	const studentInfo = !enroll ? { subjects: [] } : await getInfoFromCard(enroll.toUpperCase());
 
-	const myScheme = studentInfo.subjects
+	const myScheme = !enroll ? [] : studentInfo.subjects
 		.filter((i) => courses.find((j) => j.id === i.code))
 		.map((i) => ({
 			code: i.code,
@@ -17,12 +17,7 @@ app.get("/", async (c) => {
 			date: courses.find((j) => j.id === i.code)?.date as string,
 			time: courses.find((j) => j.id === i.code)?.time as string,
 		}))
-		.sort(
-			(a, b) =>
-				(new Date(a.date) > new Date(b.date) ? 1 : -1) ,
-		);
-
-		console.log({myScheme});
+		.sort((a, b) => (new Date(a.date) > new Date(b.date) ? 1 : -1));
 
 	return c.html(
 		<html lang="en">
@@ -38,17 +33,18 @@ app.get("/", async (c) => {
 						<h1>My Scheme</h1>
 						<h2>ZHCET Exam Scheme</h2>
 					</hgroup>
+					{/* biome-ignore lint/a11y/useSemanticElements: <explanation> */}
+					<a role="button" href="https://forms.gle/32u5JZ9Jmv1FD43v7" class="secondary p-0 float-right -mt-12">Report Issue</a>
 
-					<form action="/">
-						<label for="enrollment">Enter your enrollment number</label>
+					<form
+						action="/"
+						onload="localStorage.getItem('enroll') && window.location.href += '?enroll=' + localStorage.getItem('enroll')"
+					>
+						<label for="enroll">Enter your enrollment number</label>
 						{/* biome-ignore lint/a11y/noRedundantRoles: <explanation> */}
 						{/* biome-ignore lint/a11y/useSemanticElements: <explanation> */}
 						<fieldset role="group">
-							<input
-								type="search"
-								name="enrollment"
-								placeholder="e.g, GP4847"
-							/>
+							<input type="search" name="enroll" id="enroll" placeholder="e.g, GP4847" onchange="localStorage.setItem('enroll', this.value)" value={enroll} />
 							<button
 								type="submit"
 							// onclick="window.location.href += ('?enroll=' + document.querySelector('input').value) "
@@ -58,7 +54,7 @@ app.get("/", async (c) => {
 						</fieldset>
 					</form>
 
-					<table >
+					<table style={{ display: !enroll ? "none" : "table" }}>
 						<caption class="text-2xl font-bold my-10">Scheme</caption>
 						<thead>
 							<tr>
@@ -90,6 +86,12 @@ app.get("/", async (c) => {
 						</small>
 					</footer>
 				</main>
+				{enroll && <form action="https://ctengg.amu.ac.in/web/reg_record.php" method="post" target="_blank" >
+					<input type="hidden" name="fac" value={enroll} />
+					<input type="hidden" name="sem" value={new Date().getMonth() > 6 ? "odd" : "even"} />
+					<input type="hidden" name="submit" value="Download" />
+					<button type="submit" class="mb-10">Download Registeration Card</button>
+				</form>}
 			</body>
 		</html>,
 	);
